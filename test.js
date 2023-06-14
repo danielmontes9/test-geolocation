830d7a744f2f5b07268e33face7a77d43ea5e16a
Daniel Montes <gdanielmontesh9@gmail.com>
2023-06-14 14:01:00 -0600

    init test

---
+++ test.js
@@ -0,0 +1,18 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+
+        console.log('Latitud:', latitude);
+        console.log('Longitud:', longitude);
+    }, function (error) {
+        console.log('Error al obtener la ubicación:', error.message);
+    });
+} else {
+    console.log('El navegador no soporta la geolocalización.');
+}
+
+
+
+var userAgent = navigator.userAgent;
+console.log('User Agent:', userAgent);
