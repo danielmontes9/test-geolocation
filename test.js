75ec2bb46ef082184ee68c1e6bb72541acca2235
Daniel Montes <gdanielmontesh9@gmail.com>
2023-06-14 18:19:45 -0600

    add error case

--- test.js
+++ test.js
@@ -13,13 +13,35 @@ if (navigator.geolocation) {
         element_lon.innerHTML = `<p>Longitud: ${longitude}</p>`;
 
     }, function (error) {
-        console.log('Error al obtener la ubicación:', error.message);
 
         var element_lon = document.querySelector('.container_error');
-        element_lon.innerHTML = `<p>Error, no estoy funcionando :(</p>`;
+
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                console.log("El usuario negó el permiso para obtener la ubicación.");
+                element_lon.innerHTML = `<p>El usuario negó el permiso para obtener la ubicación.</p>`;
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.log("La información de ubicación no está disponible.");
+                element_lon.innerHTML = `<p>La información de ubicación no está disponible.</p>`;
+                break;
+            case error.TIMEOUT:
+                console.log("La solicitud para obtener la ubicación ha expirado.");
+                element_lon.innerHTML = `<p>La solicitud para obtener la ubicación ha expirado.</p>`;
+                break;
+            default:
+                console.log("Se produjo un error desconocido al obtener la ubicación.");
+                element_lon.innerHTML = `<p>Se produjo un error desconocido al obtener la ubicación.</p>`;
+                break;
+        }
+
+
+        
     });
 } else {
     console.log('El navegador no soporta la geolocalización.');
+    var element_lon = document.querySelector('.container_error');
+    element_lon.innerHTML = `<p>El navegador no soporta la geolocalización.</p>`;
 }
 
 
